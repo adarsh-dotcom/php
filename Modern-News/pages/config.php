01692a01de692991a931bf706a586b60f2062b49
adarsh-dotcom <107241319+adarsh-dotcom@users.noreply.github.com>
2025-05-12 18:52:17 +0530

    Delete Modern-News directory

--- Modern-News/pages/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "news-site";
-$port = 3307;
-
-$conn = mysqli_connect($servername,$dbUsername,$dbPassword,$dbName,$port);
-?>
